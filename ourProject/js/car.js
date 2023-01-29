df5ff12cf07e482adc4b5669acbed79d0dd6a317
22acarlson <ajcarlsonlake@gmail.com>
2023-01-29 12:51:56 -0600

    this is homepageish

--- ourProject/js/car.js
+++ ourProject/js/car.js
@@ -10,6 +10,12 @@ const CANVAS2_WIDTH = canvas2.width = 640;
 const CANVAS2_HEIGHT = canvas2.height = 640;
 ctx2.imageSmoothingEnabled = false;
 
+const canvas3 = document.getElementById('canvas3');
+const ctx3 = canvas3.getContext('2d');
+const CANVAS3_WIDTH = canvas3.width = 640;
+const CANVAS3_HEIGHT = canvas3.height = 640;
+ctx2.imageSmoothingEnabled = false;
+
 const playerImage = new Image();
 playerImage.src = "assets/trafficSprites.png";
 const crossingRoadBackgroundImage = new Image();
@@ -30,6 +36,12 @@ var initialX = 299
 var initialY = CANVAS_HEIGHT-118
 var player2X = initialX;
 var player2Y= initialY;
+
+var initial2X = 299
+var initial2Y = CANVAS_HEIGHT-118
+var player3X = initialX;
+var player3Y= initialY;
+
 
 var setSize = 128;
 
@@ -63,23 +75,6 @@ function userMovement() {
     })
 }
 
-function animatedMovement() {
-    
-    // if(player2Y > 0) {
-    //     player2Y = player2Y - setSize/12;
-    // }
-    // if(player2X < CANVAS_WIDTH-setSize) {
-    //     player2X = player2X + setSize/12;
-    // }
-    // if(player2Y < CANVAS_HEIGHT-setSize) {
-    //     player2Y = player2Y + setSize/12;
-    // }
-    // if(player2X > 0) {
-    //     player2X = player2X - setSize/12;
-    // }
-}
-
-userMovement()
 let armed = true
 function reset() {
     location.reload()
@@ -108,30 +103,74 @@ function movement() {
 }
 }
 
+function movement2() {
+    pause = false
+    let left=true
+    if(armed = true) {
+    setInterval(function(){
+        if(player3Y >= 0.00 && player3X > 234 && left === true && pause === false) {
+            player3Y = player3Y -16
+            player3X = player3X - 16
+        } else if(player3Y >= 95.33333333333333333333333333333333 && pause === false) {
+            player3Y = player3Y -16
+            left = false
+        } else if(player3Y >= 0 && player3X < 299 && left === false && pause === false) {
+            player3Y = player3Y -16
+            player3X = player3X + 16
+        } else if(player3Y > 0 && pause === false) {
+            player3Y = player3Y -16
+        } if(player3Y == 1) {
+            player3Y = CANVAS_HEIGHT-118
+            armed = false
+        }
+        }, 50);
+}
+}
 
 
 
 
 
 function car(animation) {
-ctx2.drawImage(playerImage,frameXSecondFish*frameSize, animation*frameSize, frameSize, frameSize, player2X, player2Y, setSize, setSize);
+ctx.drawImage(playerImage,frameXSecondFish*frameSize, animation*frameSize, frameSize, frameSize, player2X, player2Y, setSize, setSize);
 }
 
+function car2(animation) {
+ctx2.drawImage(playerImage,frameXSecondFish*frameSize, animation*frameSize, frameSize, frameSize, player3X, player3Y, setSize, setSize);
+}
+    
+
 function truck(x, y, animation) {
+ctx.drawImage(playerImage,frameXSecondFish*frameSize, animation*frameSize, frameSize, frameSize, x, y, setSize, setSize);
+}
+
+function truck2(x, y, animation, frameXSecondFish) {
 ctx2.drawImage(playerImage,frameXSecondFish*frameSize, animation*frameSize, frameSize, frameSize, x, y, setSize, setSize);
 }
 
 function road(x, y, animation) {
-ctx.drawImage(crossingRoadBackgroundImage,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
+ctx3.drawImage(crossingRoadBackgroundImage,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
 }
 
 function secondRoad(x, y, animation) {
 ctx.drawImage(passingRoad,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
 }
+function secondRoad2(x, y, animation) {
+ctx2.drawImage(passingRoad,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
+}
 
 function grass(x, y, animation) {
 ctx.drawImage(grassBackgroundImage,frameXSecondFish*frameSize3, animation*frameSize3, frameSize3, frameSize3, x, y, setSize, setSize);
 }
+
+function grass2(x, y, animation) {
+ctx2.drawImage(grassBackgroundImage,frameXSecondFish*frameSize3, animation*frameSize3, frameSize3, frameSize3, x, y, setSize, setSize);
+}
+
+function grass3(x, y, animation) {
+ctx3.drawImage(grassBackgroundImage,frameXSecondFish*frameSize3, animation*frameSize3, frameSize3, frameSize3, x, y, setSize, setSize);
+}
+
 
 
 
@@ -142,19 +181,36 @@ function kindaLikeTheBackgroundLayer() {
         }
     }
 }
+
+function kindaLikeTheBackgroundLayer3() {
+    for(i=0; i<CANVAS_WIDTH; i++) {
+        for(j=0; j<CANVAS_HEIGHT; j++) {
+            grass3(i*setSize, j*setSize, 0)
+        }
+    }
+}
 function notReallyTheTrueBackgroundLayer() {
-    // road(0*setSize, 0*setSize, 0)
         for(j=0; j<CANVAS_HEIGHT; j++) {
             secondRoad(2*setSize-7, j*setSize, 0)
         }
 
 }
 
-function layerPassRoad() { 
+function notReallyTheTrueBackgroundLayer3() {
+        road(0*setSize, 0*setSize, 0)
+}
+
+function kindaLikeTheBackgroundLayer2() {
     for(i=0; i<CANVAS_WIDTH; i++) {
         for(j=0; j<CANVAS_HEIGHT; j++) {
-            passingRoad(i*setSize, j*setSize, 0)
+            grass2(i*setSize, j*setSize, 0)
         }
+    }
+}
+
+function layerPassRoad2() { 
+    for(j=0; j<CANVAS_HEIGHT; j++) {
+        secondRoad2(2*setSize-7, j*setSize, 0)
     }
 }
 
@@ -165,7 +221,12 @@ function layer7() {
         
 }
 
-
+function layer72() {
+    truck2(2.35*setSize, 1.50*setSize, 5, 1)
+    truck2(2.35*setSize, 2.31*setSize, 6, 1)
+    car2(0)
+    
+}
 function scene0 () {
     layer7();
 }
@@ -177,23 +238,42 @@ function scene1() {
 
 function scene2() {
     layerPassRoad();
+}
 
+function scene22() {
+    kindaLikeTheBackgroundLayer2();
+    layerPassRoad2();
+    layer72();
+}
+
+function scene33() {
+    kindaLikeTheBackgroundLayer3();
+    notReallyTheTrueBackgroundLayer3();
 }
 
 
-function drawScreen() {
+function drawScreen2() {
     ctx2.clearRect(0, 0, CANVAS2_WIDTH, CANVAS2_HEIGHT);
-    scene0();
+    scene22();
     gameFrame++;
-    requestAnimationFrame(drawScreen);
+    requestAnimationFrame(drawScreen2);
 };
 
-function drawScreen2() {
+function drawScreen3() {
+    ctx3.clearRect(0, 0, CANVAS3_WIDTH, CANVAS3_HEIGHT);
+    scene33();
+    gameFrame++;
+    requestAnimationFrame(drawScreen3);
+};
+
+function drawScreen() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_WIDTH);
     scene1();
+    scene0();
     gameFrame++;
-    requestAnimationFrame(drawScreen2)
+    requestAnimationFrame(drawScreen)
 }
 drawScreen();
 drawScreen2();
+drawScreen3();
