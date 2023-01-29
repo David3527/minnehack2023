dc7de9daf18721932ea6b4f20ba345bfc2afc378
helge409 <helge409@d.umn.edu>
2023-01-28 21:27:23 -0600

    new road

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,10 @@
+<nav class="">
+<p class="font-bold text-2xl">MinneDrive</p>
+<div class="border-4 border-indigo-500/100 ..."></div>
+</nav>
+
+
+
+
+
+<style> </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
     import '../app.css';
     import Sidebar from '$lib/components/Sidebar.svelte';
+    import Navigation from '../lib/components/Navigation.svelte';
 </script>
 
-<div class="p-4 max-w-screen-xl mx-auto">
+<div class="p-4 space-y-4 max-w-screen-xl mx-auto">
+    <Navigation/>
+
     <div class="grid gap-4 grid-cols-[min-content,1fr]">
         <aside class="sticky top-0 whitespace-nowrap">
             <Sidebar/>
