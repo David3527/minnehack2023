52567ee4175b685d4a74f9b97721aef66748bc93
22acarlson <ajcarlsonlake@gmail.com>
2023-01-29 02:50:01 -0600

    this be animated with reset though

--- animationCanvasTest/js/car.js
+++ animationCanvasTest/js/car.js
@@ -12,10 +12,12 @@ ctx2.imageSmoothingEnabled = false;
 
 const playerImage = new Image();
 playerImage.src = "assets/trafficSprites.png";
-const backgroundImage = new Image();
-backgroundImage.src = "assets/road.png";
+const crossingRoadBackgroundImage = new Image();
+crossingRoadBackgroundImage.src = "assets/road.png";
 const grassBackgroundImage = new Image();
 grassBackgroundImage.src = "assets/grass.png";
+const passingRoad = new Image();
+passingRoad.src = "assets/passingroad.png"
 
 const frameSize = 64;
 const frameSize2 = 320;
@@ -24,15 +26,17 @@ var gameFrame = 0;
 var skipFrames = 10;
 
 initObjects();
-
-var player2X = 299;
-var player2Y= CANVAS_HEIGHT-118;
+var initialX = 299
+var initialY = CANVAS_HEIGHT-118
+var player2X = initialX;
+var player2Y= initialY;
 
 var setSize = 128;
 
 function  initObjects() {
     frameXSecondFish = 0;
     frameYSecondFish = 0;
+    
 }
 
 function userMovement() {
@@ -40,6 +44,7 @@ function userMovement() {
         if(e.key.toLowerCase() === 'w') {
             if(player2Y > 0) {
                 player2Y = player2Y - setSize/12;
+                console.log(player2Y)
             }
         } else if(e.key.toLowerCase() === 'd') {
             if(player2X < CANVAS_WIDTH-setSize) {
@@ -52,6 +57,7 @@ function userMovement() {
         } else if(e.key.toLowerCase() === 'a') {
             if(player2X > 0) {
                 player2X = player2X - setSize/12;
+                console.log(player2X)
             }
         }
     })
@@ -73,13 +79,39 @@ function animatedMovement() {
     // }
 }
 
-function movement() {
-    setInterval(function(){
-        if(player2Y > 0) {
-            player2Y = player2Y -16
-        }       
-      }, 50);
+userMovement()
+let armed = true
+function reset() {
+    player2X = initialX
+    player2Y = initialY
+    pause = true
 }
+function movement() {
+    pause = false
+    let left=true
+    if(armed = true) {
+    setInterval(function(){
+        if(player2Y >= 0 && player2X > 222.33333333333333333333333333333 && left === true && pause === false) {
+            player2Y = player2Y -16
+            player2X = player2X - 16
+        } else if(player2Y >= 95.33333333333333333333333333333333 && pause === false) {
+            player2Y = player2Y -16
+            left = false
+        } else if(player2Y >= 0 && player2X < 299 && left === false && pause === false) {
+            player2Y = player2Y -16
+            player2X = player2X + 16
+        } else if(player2Y > 0 && pause === false) {
+            player2Y = player2Y -16
+        } if(player2Y == 1) {
+            player2Y = CANVAS_HEIGHT-118
+            armed = false
+        }
+        }, 50);
+}
+}
+
+
+
 
 
 
@@ -92,7 +124,11 @@ ctx2.drawImage(playerImage,frameXSecondFish*frameSize, animation*frameSize, fram
 }
 
 function road(x, y, animation) {
-ctx.drawImage(backgroundImage,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
+ctx.drawImage(crossingRoadBackgroundImage,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
+}
+
+function secondRoad(x, y, animation) {
+ctx.drawImage(passingRoad,frameXSecondFish*frameSize2, animation*frameSize2, frameSize2, frameSize2, x, y, setSize*5, setSize*5);
 }
 
 function grass(x, y, animation) {
@@ -109,22 +145,29 @@ function kindaLikeTheBackgroundLayer() {
     }
 }
 function notReallyTheTrueBackgroundLayer() {
-    road(0*setSize, 0*setSize, 0)
+    // road(0*setSize, 0*setSize, 0)
+        // for(j=0; j<CANVAS_HEIGHT; j++) {
+            // secondRoad(2*setSize, 2*setSize, 0)
+        // }
+
 }
 
 
 function layer7() {
         car(0)
-        // truck(2.35*setSize, 1.75*setSize, 5)
-        // truck(2.35*setSize, 2.56*setSize, 6)
+        truck(2.35*setSize, 1.50*setSize, 5)
+        truck(2.35*setSize, 2.31*setSize, 6)
         
 }
 
 
 function scene0 () {
-    kindaLikeTheBackgroundLayer()
-    notReallyTheTrueBackgroundLayer()
     layer7();
+}
+
+function scene1() {
+    kindaLikeTheBackgroundLayer()
+    // notReallyTheTrueBackgroundLayer()
 }
 
 
