75bdd4e3830bfdf173177c460acac715cee65a5b
helge409 <helge409@d.umn.edu>
2023-01-29 13:44:58 -0600

    styles

--- ourProject/js/question.js
+++ ourProject/js/question.js
@@ -1,1 +1,23 @@
-document.element.getID().eventListener()
+function answer() {
+    document.getElementById('awnser').addEventListener('click', (e)=> {
+        correctAwnser = document.getElementById('answer')
+        correctAnswer.classList.add('correctAnswer')
+    }) 
+    document.getElementById('awnser').addEventListener('click', (e)=> {
+        correctAwnser1 = document.getElementById('answer1')
+        correctAnswer1.classList.add('correctAnswer')
+    }) 
+    document.getElementById('awnser').addEventListener('click', (e)=> {
+        correctAwnser2 = document.getElementById('answer2')
+    correctAnswer2.classList.add('correctAnswer')
+    }) 
+    
+}
+
+answer();
+
+function fourthToggleFunction() {
+        document.getElementById("accountDropdown").classList.toggle("fourthShow");
+    }
+
+
