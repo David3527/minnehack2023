d220da94137a52a4b6a932fcee41268b7fcacac8
helge409 <helge409@d.umn.edu>
2023-01-29 09:49:04 -0600

    temp fix canvas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
 <script>
     import '../app.css';
-    import '../../animationCanvasTest/style.css';
+    //import '$lib/components/animationCanvasTest/style.css';
     import Sidebar from '$lib/components/Sidebar.svelte';
     import Navigation from '$lib/components/Navigation.svelte';
-    import Car from '$lib/components/Car.svelte';
+    //import Car from '$lib/components/Car.svelte';
+    //import Helper from '$lib/components/animationCanvasTest/js/car.js';
+    //let helper = new Helper();
     
 </script>
 
 <div class="p-4 max-w-screen-xl mx-auto">
     <Navigation/>
     <div class="grid gap-4 grid-cols-[min-content,1fr]">
-        <Car/>
+         <!-- <Car/>  -->
         <aside class="sticky top-0 whitespace-nowrap">
             <Sidebar/>
         </aside>
